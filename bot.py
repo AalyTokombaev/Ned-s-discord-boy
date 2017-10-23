fc4574980c1d25c475d37ec305146ceeee06beb9
OlderNed <alexiver96@hotmail.com>
2017-10-23 18:31:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
-import asyncio
 from string import printable
+from random import choice, randint
 from PIL import Image, ImageFilter
 
+import urbandictionary as UD
 
 import discord
-from discord.ext import commands
 from discord.ext.commands import Bot
-from discord.utils import find, get
 
 bot = Bot(command_prefix='?')
 
@@ -33,23 +32,34 @@ async def on_member_join(member: discord.Member):
 @bot.event
 async def on_member_remove(member: discord.Member):
     channel = member.guild.system_channel
-    await channel.send(f'{member.mention} has left us.')
+    await channel.send(f"{member.mention} has left us.")
+
+
+@bot.listen()
+async def on_message(message):
+    if message.content == "Freddy you there dude?":
+        await message.channel.send(f"Yeah, {message.author.name} I'm here.")
 
 
 @bot.command()
-async def set_channel(ctx, channel_name: str):
+async def set_channel(ctx, channel_name=None):
     """Set's the servers `system_channel`. This is currently used to send messages upon client events."""
     # TODO: Change this function to not use the system_channel as a destination to welcomes, since it might
     #  (and probably is
     # being used) be used for other purposes.
     # TODO: Set up some sort of options file so we don't have to call this every time the bot is launched.
-    find_channel = discord.utils.get(ctx.message.guild.text_channels, name=channel_name)
+
+    if not channel_name:
+        find_channel = ctx.message.channel
+    else:
+        find_channel = discord.utils.get(ctx.message.guild.text_channels, name=channel_name)
+
     try:
         await ctx.message.guild.edit(system_channel=find_channel)
     except:
         await ctx.message.channel.send('I do not have the required permissions.')
         return
-    print(ctx.message.guild.system_channel)
+
     await ctx.message.channel.send(f'New default channel: {find_channel}')
 
 
@@ -62,7 +72,8 @@ async def aesthetics(ctx, *args):
 
 
 @bot.command()
-async def iter(ctx):
+async def iterate(ctx):
+    """No point to this one yet. Just message stats"""
     messages = ctx.message.channel.history(limit=None)
     channel = ctx.message.channel
     authors = {}
@@ -78,35 +89,63 @@ async def iter(ctx):
 
 
 @bot.command()
-async def needsmoreedge(ctx):
+async def needsmoreedge(ctx, edge=2):
     """"Adds much needed edge to the last posted image (still kinda buggy bear with me"""
-    messages = ctx.message.channel.history(limit=2)
+    if type(edge) != type(int):
+        try:
+            edge = int(edge)
+        except ValueError:
+            await ctx.message.chanel.send('Invalid value, must be integer.')
+            return
+    messages = ctx.message.channel.history(limit=10)
     async for message in messages:
         if message.attachments:
             for attch in message.attachments:
-               #This kinda takes an image at random, I think I'll keep the history limit=2 for the momment
-               #It seems to pick the oldest image first. If you call the command again it'll go to the next one.
-               #TODO: Figure out a way to only take the last image.
+                # This kinda takes an image at random, I think I'll keep the history limit=2 for the momment
+                # It seems to pick the lastest image first. If you call the command again it'll go to the next one.
+                # TODO: Figure out a way to only take the last image.
                 await attch.save('some_file.jpeg')
     image = Image.open('some_file.jpeg')
     if image:
         edge_image = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
+        for f in range(edge):
+            edge_image = edge_image.filter(ImageFilter.EDGE_ENHANCE_MORE)
+        # this is for another meme
+        # edge_image = image.filter(ImageFilter.UnsharpMask(radius=200, percent=500, threshold=1))
         edge_image.save('returned_file.jpeg')
         await ctx.message.channel.send(file=discord.File('returned_file.jpeg'))
 
 
+@bot.command()
+async def random_string(ctx):
+    """Generates a random string from 3 to 500 characters in length."""
+    s = ''.join([choice(printable) for f in range(randint(3, 500))])
+    await ctx.message.channel.send(s)
+
 
 @bot.command()
-async def quit(ctx):
+async def ud(ctx, search_item=None, number=0):
+    if not search_item:
+        result = UD.random()[0]
+        await ctx.channel.send(f'**{result.word.capitalize()}**\n*{result.definition}*')
+        return
+    result = UD.define(search_item)[number]
+    if result:
+        await ctx.channel.send(f"**{result.word.capitalize()}**\n*{result.definition}*")
+    else:
+        await ctx.channel.send('Not Found')
+
+
+@bot.command()
+async def disconnect(ctx):
     """Basic quit command."""
     # TODO: Make it so the admin of any given guild is allowed to use this and have a command that let's them set
     # quit permission for other users.
     if ctx.message.author.id != 138001563158446081:
         await ctx.message.channel.send("I can't let you do that..")
         return
-    await ctx.message.channel.send('Quitting...')
+    await ctx.message.channel.send('Noooo...')
     await bot.logout()
-
 
 bot.run('token')
 # I like keeping my tokens here
